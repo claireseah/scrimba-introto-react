8d7114d7319c672df4e751e02f3522e65c6f1e45
claireseah <47802340+claireseah@users.noreply.github.com>
2020-03-28 14:06:43 +0800

    Add files via upload

---
+++ React-Todo-Phase1/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox"></input>
+            <label>Option 1</label>
+            <br></br>
+            <input type="checkbox"></input>
+            <label>Option 2</label>
+            <br></br>
+            <input type="checkbox"></input>
+            <label>Option 3</label>
+        </div>
+    )
+}
+
+export default App
