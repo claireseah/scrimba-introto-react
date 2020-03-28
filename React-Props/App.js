46e7ac123266e9977e7fc1c8422152819f1878e2
claireseah <47802340+claireseah@users.noreply.github.com>
2020-03-28 14:14:30 +0800

    Add files via upload

---
+++ React-Props/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import ContactCard from "./ContactCard"
+
+function App() {
+    return (
+        <div className="contacts">
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+        </div>
+    )
+}
+
+export default App
