98c86015f1bf193ac580c07da85e924060d6aa0a
claireseah <47802340+claireseah@users.noreply.github.com>
2020-03-28 14:13:21 +0800

    Add files via upload

---
+++ React-Todo-Phase2/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TodoIteam() {
+    return (
+        <div class="todo-item">
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+        </div>
+    )
+}
+
+export default TodoIteam
